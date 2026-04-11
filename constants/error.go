a14fa593e285f4c4d7199996e884d3befbf0c1b8
Muhammad Hasnan Regard <muhammad.regard@shopee.com>
2026-04-11 20:27:17 +0700

    feat: add custom error

--- constants/error.go
+++ constants/error.go
@@ -1,7 +1,31 @@
 package constants
 
+import "net/http"
+
 var (
-	ErrFieldBadRequest = "data is not valid"
-	ErrDuplicateEmail  = "email is already registered"
-	ErrUserNotFound    = "user is not registered"
+	ErrFieldBadRequest  = "data is not valid"
+	ErrDuplicateEmail   = "email is already registered"
+	ErrUserNotFound     = "user not found"
+	ErrRequiredEmail    = "email is required"
+	ErrRequiredUsername = "username is required"
+	ErrRequiredPassword = "password is required"
+)
+
+type AppError struct {
+	StatusCode int
+	Message    string
+}
+
+func (e *AppError) Error() string {
+	return e.Message
+}
+
+func NewAppError(statusCode int, message string) *AppError {
+	return &AppError{StatusCode: statusCode, Message: message}
+}
+
+var (
+	ErrorDuplicateEmail = NewAppError(http.StatusConflict, ErrDuplicateEmail)
+	ErrorRequiredEmail  = NewAppError(http.StatusBadRequest, ErrRequiredEmail)
+	ErrorUserNotFound   = NewAppError(http.StatusNotFound, ErrUserNotFound)
 )
